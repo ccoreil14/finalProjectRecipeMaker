33067187155e5443e63764bb726afb747006dcda
Daniel Carapia <toonlink4943@gmail.com>
2017-03-06 20:01:25 -0700

    added more things christian told me to do

--- app/src/main/java/drawapptutorial/com/example/rem/AddRecipeForm.java
+++ app/src/main/java/drawapptutorial/com/example/rem/AddRecipeForm.java
@@ -2,12 +2,27 @@ package drawapptutorial.com.example.rem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class AddRecipeForm extends AppCompatActivity {
+public class AddRecipeForm extends AppCompatActivity implements View.OnClickListener {
+
+    Button addRecipeBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_recipe);
+
+        addRecipeBtn = (Button) findViewById(R.id.addRecipeBtn);
+        addRecipeBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.addRecipeBtn){
+            //do stuff
+            finish();
+        }
     }
 }
